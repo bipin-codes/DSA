523286d84fe419d1440bdf774f13ce9f9b3f475f
Bipin Gosain <bipincodes@outlook.com>
2022-07-12 07:21:02 +0530

    Array reversal

---
+++ DataStructures/arrays/strings_array.js
@@ -0,0 +1,31 @@
+// INTERVIEW STRING QUESTION SHOULD BE TREATED AS ARRAY QUESTIONS
+
+// FUNCTION to rever a string
+
+function reverse(str) {
+  if (!str || str.length < 2 || typeof str != "string")
+    throw new Error("Ïnvalid String");
+
+  let lp = 0,
+    up = str.length;
+  const strArr = str.split("");
+
+  // O(n) -> Complexity
+  while (lp < up) {
+    let val = strArr[lp];
+    strArr[lp] = strArr[up];
+    strArr[up] = val;
+    lp++;
+    up--;
+  }
+
+  return strArr.join("");
+}
+
+function reverseAlt(str) {
+  return [...str].reverse().join("");
+}
+
+console.log(reverse("bipin"));
+console.log(reverse("bipin gosain"));
+console.log(reverseAlt("good morning"));
